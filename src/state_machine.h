cf950ce794ac591c0b217ae930a8bdc7d62418b3
potatislc <lucalbin@gmail.com>
2026-05-12 19:11:27 +0200

    Add change state

--- src/state_machine.h
+++ src/state_machine.h
@@ -1,40 +1,28 @@
 #pragma once
 #include <algorithm>
 #include <array>
+#include <cassert>
 #include <concepts>
+#include <new>
 
 namespace platformer {
-    class State;
-
-    class StateMachineBase {
-    public:
-        explicit StateMachineBase(State* buffer_ptr) : buffer_ptr(buffer_ptr) {
-        }
-
-    private:
-        State* buffer_ptr{};
-    };
-
-    namespace concepts {
-        template<typename T>
-        concept StateMachine = std::derived_from<T, StateMachineBase>;
-    }
+    class StateMachineBase;
 
     class State {
     public:
-        template<concepts::StateMachine T>
-        explicit State(T& state_machine) : state_machine{state_machine} {
-        }
+        friend StateMachineBase;
+
+        State() = default;
 
         virtual ~State() = default;
 
-        State(const State&) = delete;
+        State(const State&) = default;
 
-        State& operator=(const State& other) = delete;
+        State& operator=(const State& other) = default;
 
-        State(State&&) = delete;
+        State(State&&) = default;
 
-        State& operator=(State&&) = delete;
+        State& operator=(State&&) = default;
 
         virtual void enter();
 
@@ -46,13 +34,60 @@ namespace platformer {
 
         virtual void exit();
 
-    protected:
-        StateMachineBase& state_machine;
+        [[nodiscard]] const StateMachineBase& get_state_machine() const {
+            assert(state_machine != nullptr);
+            return *state_machine;
+        }
+
+        [[nodiscard]] StateMachineBase& get_state_machine() {
+            assert(state_machine != nullptr);
+            return *state_machine;
+        }
+
+    private:
+        StateMachineBase* state_machine{};
     };
 
     namespace concepts {
         template<typename T>
         concept State = std::derived_from<T, platformer::State>;
+    }
+
+    class StateMachineBase {
+    public:
+        explicit StateMachineBase(State* buffer_ptr) : current_state(buffer_ptr) {
+            create_state<State>();
+        }
+
+        ~StateMachineBase() {
+            destroy_state();
+        }
+
+        template<concepts::State T, typename... Args>
+        void change_state(Args&&... args) {
+            destroy_state();
+            create_state<T>(std::forward<Args>(args)...);
+        }
+
+    private:
+        template<concepts::State T, typename... Args>
+        void create_state(Args&&... args) {
+            new(current_state) T(std::forward<Args>(args)...);
+            current_state->state_machine = this;
+            current_state->enter();
+        }
+
+        void destroy_state() const {
+            current_state->exit();
+            current_state->~State();
+        }
+
+        State* const current_state{};
+    };
+
+    namespace concepts {
+        template<typename T>
+        concept StateMachine = std::derived_from<T, StateMachineBase>;
     }
 
     template<concepts::State... States>
