df96b9dcfd6ece065aeb09fc4acef72780215c60
potatislc <lucalbin@gmail.com>
2026-05-12 19:29:27 +0200

    Fix: Assign current state

--- src/state_machine.h
+++ src/state_machine.h
@@ -24,15 +24,20 @@ namespace platformer {
 
         State& operator=(State&&) = default;
 
-        virtual void enter();
+        virtual void enter() {
+        }
 
-        virtual void update(float delta);
+        virtual void update(float delta) {
+        }
 
-        virtual void physics_update(float delta);
+        virtual void physics_update(float delta) {
+        }
 
-        virtual void draw();
+        virtual void draw() {
+        }
 
-        virtual void exit();
+        virtual void exit() {
+        }
 
         [[nodiscard]] const StateMachineBase& get_state_machine() const {
             assert(state_machine != nullptr);
@@ -59,9 +64,17 @@ namespace platformer {
             create_state<State>();
         }
 
-        ~StateMachineBase() {
+        virtual ~StateMachineBase() {
             destroy_state();
         }
+
+        StateMachineBase(const StateMachineBase&) = delete;
+
+        StateMachineBase& operator=(const StateMachineBase&) = delete;
+
+        StateMachineBase(StateMachineBase&&) = delete;
+
+        StateMachineBase& operator=(StateMachineBase&&) = delete;
 
         template<concepts::State T, typename... Args>
         void change_state(Args&&... args) {
@@ -72,17 +85,17 @@ namespace platformer {
     private:
         template<concepts::State T, typename... Args>
         void create_state(Args&&... args) {
-            new(current_state) T(std::forward<Args>(args)...);
+            current_state = new(current_state) T(std::forward<Args>(args)...);
             current_state->state_machine = this;
             current_state->enter();
         }
 
         void destroy_state() const {
             current_state->exit();
-            current_state->~State();
+            std::destroy_at(current_state);
         }
 
-        State* const current_state{};
+        State* current_state{};
     };
 
     namespace concepts {
@@ -91,7 +104,7 @@ namespace platformer {
     }
 
     template<concepts::State... States>
-    class StateMachine : public StateMachineBase {
+    class StateMachine final : public StateMachineBase {
     public:
         static constexpr size_t STATE_BUFFER_SIZE{std::max({sizeof(State), sizeof(States)...})};
         static constexpr size_t STATE_BUFFER_ALIGN{std::max({alignof(State), alignof(States)...})};
